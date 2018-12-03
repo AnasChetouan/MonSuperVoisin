72bc6d8a5c4cc0d56807ee3b14f022d571515846
Marco347 <36471442+Marco347@users.noreply.github.com>
2018-12-03 20:56:46 +0100

    Update list.php

--- view/bien/list.php
+++ view/bien/list.php
@@ -8,7 +8,8 @@
             $idBien = htmlspecialchars($b->getIdBien());
             $loginProprio = htmlspecialchars(ModelMembre::getLoginById(($b->getIdProprio())));
             $tabNotes = ModelCommentaire::selectAllCommByIdProduit($idBien);
-
+            $estDispo = htmlspecialchars($b->getEstDispo());
+            if($estDispo == 1){
             echo '<div class="produit">';
             echo '<br/> <p> <b><img src='.$lienPhoto.' alt="photo bien" height="50%" width="50%" ></b>'.'<br/>  </p>';
             echo '<br/> <p>'.'<b>'.$titreHTML.'</b>'.'<br/>  </p>';
@@ -21,14 +22,15 @@
 	                }
             }
             else{
-            	echo '<b>Pas encore noté </b>'.'<br/>'.'<br/>';
+            	echo '<b>Pas encore notÃ© </b>'.'<br/>'.'<br/>';
             }
             
             //echo '<br/> <p>'.'<b>'.$descHTML.'</b>'.'<br/>  </p>';
-            echo 'Propriétaire : '.$loginProprio;
+            echo 'PropriÃ©taire : '.$loginProprio;
             echo '<br/>'.'<br/>'.'<a href="index.php?controller=bien&action=read&id='.$idBien.'"> Detail objet </a>';
             echo '</div>';
             echo '</a>';
+            }
         }
     ?>
 </div>
