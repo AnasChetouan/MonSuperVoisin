5d5d1bdcf7bde7e944f952dbf53dd848aa31d04a
AnasChetouan <anas.chetouan@gmail.com>
2018-12-09 20:29:34 +0100

    Delete page_introuvable.php

--- view/membre tes/page_introuvable.php
+++
@@ -1,3 +0,0 @@
-<div id="bloc_erreur">
-  <p> <h3> La page renseigné dans l'URL est introuvable, veuillez vérifier si vous avez bien écrit ce que vous voulez dans la barre d'adresse, si c'est le cas vous pouvez contacter notre service de gestion du site à la page contact</h3></p>
-</div>
