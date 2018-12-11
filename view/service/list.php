72b3d074a547c82e17ef972543adc1ad8ec265cd
Marco347 <36471442+Marco347@users.noreply.github.com>
2018-12-11 16:45:38 +0100

    Add files via upload

--- view/service/list.php
+++ view/service/list.php
@@ -1,6 +1,28 @@
 <div class="produits">
     
     <?php
+    
+        if(!empty($_SESSION['login'])){
+            $ville = " pres de ".ModelMembre::getVilleByLogin($_SESSION['login']);
+        }
+        else $ville = "";
+    
+    echo '
+        <form action="index.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="controller" value="Service">
+        <input type="hidden" name="action" value="rechercheService">
+                 <div id="recherche"> Rechercher un Service'.$ville.' :
+                      <input type="text" name="nom" required placeholder="Exemple : Babysitting" />
+                      </br>
+                      Entre : 
+                      <input type="number" name="prix1" min="0" placeholder=0> € et 
+                      <input type="number" name="prix2" min="0" placeholder=100> € / heures
+                      </br>
+                      </br>
+                 <input type="submit" value="Rechercher" />
+                 </div> 
+        </form>  
+        </br>';          
         foreach($tab_s as $s){ 
             $idService = htmlspecialchars($s->getIdService());
             $motClef = htmlspecialchars($s->getMotClef());
@@ -12,7 +34,7 @@
             echo '<p>'.'<b>'.$motClef.'</b>'.'<br/>  </p>';
             
             // <p>'.'<b>'.$descHTML.'</b>'.'<br/>  </p>';
-            echo 'PropriÃ©taire : '.$loginProprio;
+            echo 'Propriétaire : '.$loginProprio;
             echo '<br/>'.'<br/>'.'Tarif horaire : '.$tarif." â‚¬";
             echo '<br/>'.'<br/>'.'<a href="index.php?controller=service&action=read&idService='.$idService.'"><button> Plus d\'infos </button></a>';
             echo '</div>';
