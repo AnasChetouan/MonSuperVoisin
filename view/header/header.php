5876cf87057b93c9eca7e06ef6a5cd8a4c58e2f6
Marco347 <36471442+Marco347@users.noreply.github.com>
2018-12-16 11:14:55 +0100

    notif

--- view/header/header.php
+++ view/header/header.php
@@ -35,19 +35,21 @@
                         echo "</a>";
                     	echo "<ul class='submenu'>";
 						echo '<li><a href="index.php?controller=membre&action=read&login='.$_SESSION['login'].'">Mon profil</a></li>';
-						echo '<li><a href="index.php?controller=membre&action=deconnect">Se dÃ©connecter</a></li>';
+						echo '<li><a href="index.php?controller=membre&action=deconnect">Se déconnecter</a></li>';
                                                 if (Session::is_admin()){
                                                 echo '<li><a href="index.php?controller=membre&action=gestionAnnonces">Gestion des Annonces</a></li>';
+                                                echo '<li><a href="index.php?controller=notification&action=readAll">Gestion des Notifications</a></li>';
                                                 }
+                                                else echo '<li><a href="index.php?controller=notification&action=readAll">Mes Messsages</a></li>';
                         echo '</ul>';
                     	echo '</li>';
                     }
+                    
+                        if((isset($_SESSION['login'])))
+                        echo '<li class="menu-contact"><a href="index.php?controller=notification&action=create">Contact</a></li>
+
+				</ul>';
                     ?>
-	
-					<li class="menu-contact"><a href="#">Contact</a></li>
-
-				</ul>
-
 
 </nav>
 
