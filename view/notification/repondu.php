0db545d192e755b262e74e5867538626332c04f9
Marco347 <36471442+Marco347@users.noreply.github.com>
2018-12-16 11:52:20 +0100

    Add files via upload

--- view/notification/repondu.php
+++ view/notification/repondu.php
@@ -1,5 +1,5 @@
 <?php
     echo "<p>Reponse Validee</p>";;
     require_once File::build_path(array("index.php"));
-    echo'<a href="index.php?controller=membre&action=readAll"> Retour </a>';
+    echo'<a href="index.php?controller=notification&action=readAll"> Retour </a>';
 ?>
