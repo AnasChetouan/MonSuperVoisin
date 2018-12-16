6402346f001e79a171cfab257a44f902f146132f
Marco347 <36471442+Marco347@users.noreply.github.com>
2018-12-16 11:51:17 +0100

    Add files via upload

--- view/notification/list.php
+++ view/notification/list.php
@@ -11,14 +11,16 @@
                 $reponse = htmlspecialchars($n->getReponse());
                 $estRegle = htmlspecialchars($n->getEstRegle());
             
-                if($estRegle == 0){
+                if($estRegle == 0 && Session::is_admin()){
                  echo '<div class="notification">';
                  echo 'Message de  : '.ModelMembre::getLoginById($idMembre);
                  echo '</br>';
                  echo $message;
                  echo '</br><a href="index.php?controller=notification&action=reponse&idNotif='.$idNotif.
                          '"> </br><button>Répondre</button> </a> </br>';
+                 echo '</div>';
                 }
+            
                 
                 if(($estRegle == 1) && (!Session::is_admin()) && ($idMembre == ModelMembre::getIdByLogin($_SESSION['login']))){
                  echo '<div class="notification">';
@@ -27,6 +29,7 @@
                  echo $reponse;
                  echo '</br><a href="index.php?controller=notification&action=delete&idNotif='.$idNotif.
                          '"> </br><button>Supprimer</button> </a> </br>';
+                 echo '</div>';
                 }
                 
             }
